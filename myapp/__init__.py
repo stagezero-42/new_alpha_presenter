205dd359396bbf239279439d612fe74055ee6496
stagezero-42 <david.rout@stagezero.com.au>
2025-05-28 14:46:33 +1000

    Text overlay option, Editor working, json updates working, minor GUI updates

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,6 +1,7 @@
 # This is only an empty file Empty or contains package metadata (e.g., version). Turns this folder into a package
 # myapp/__init__.py
-__version__ = "0.3.0" # Error printing changed to logging
+__version__ = "0.3.4" # New Text overlay option, Editor working, json updates working
+# 5. Slide / Text Integration next.
 # Next: The Show Button nees to change to a play pause function.
 # Hide display button needs to pause / continue playing on click.
 # text overlay
